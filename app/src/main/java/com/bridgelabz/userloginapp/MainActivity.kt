b2849c9807ea18e7d1a06563f75e215189d6d4d5
ksoundarya4 <ksoundarya4@gmail.com>
2020-03-18 17:49:17 +0530

    [soundarya]Add. Added UserLoginApp

---
+++ app/src/main/java/com/bridgelabz/userloginapp/MainActivity.kt
@@ -0,0 +1,12 @@
+doo notes package com.bridgelabz.userloginapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
