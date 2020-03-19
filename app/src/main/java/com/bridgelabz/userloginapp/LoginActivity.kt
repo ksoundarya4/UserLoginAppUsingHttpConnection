c81e73b396f4073b7d7eda73c38079e529197067
ksoundarya4 <ksoundarya4@gmail.com>
2020-03-19 10:56:02 +0530

    [soundarya]Refactor. Added click listeners to login and register button in LoginActivity.kt

--- app/src/main/java/com/bridgelabz/userloginapp/LoginActivity.kt
+++ app/src/main/java/com/bridgelabz/userloginapp/LoginActivity.kt
@@ -1,12 +1,58 @@
 package com.bridgelabz.userloginapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class LoginActivity : AppCompatActivity() {
+
+    private val emailEditText by lazy {
+        findViewById<EditText>(R.id.login_email)
+    }
+
+    private val passwordEditText by lazy {
+        findViewById<EditText>(R.id.login_password)
+    }
+
+    private val loginButton by lazy {
+        findViewById<Button>(R.id.button_login)
+    }
+
+    private val registrationButton by lazy {
+        findViewById<Button>(R.id.button_register)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        setButtonClickListeners()
+    }
+
+    private fun setButtonClickListeners() {
+        setLoginButtonListener()
+        setRegistrationButtonListener()
+    }
+
+
+    private fun setLoginButtonListener() {
+        loginButton.setOnClickListener {
+            val email = emailEditText.text.toString()
+            val password = passwordEditText.text.toString()
+        }
+    }
+
+    private fun setRegistrationButtonListener() {
+        registrationButton.setOnClickListener {
+            navigateToReciterActivity()
+        }
+    }
+
+    private fun navigateToReciterActivity() {
+        val intent = Intent(this, RegisterActivity::class.java)
+        this.finish()
+        startActivity(intent)
     }
 }
