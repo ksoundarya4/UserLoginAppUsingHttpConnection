194f2e7188fc468b4d9cccfa98a9591e1fcc5ec9
ksoundarya4 <ksoundarya4@gmail.com>
2020-03-19 16:37:00 +0530

    [soundarya]Add. Added UserLogin.kt

---
+++ app/src/main/java/com/bridgelabz/userloginapp/UserLogin.kt
@@ -0,0 +1,11 @@
+/**
+ * UserLoginApp
+ * @description UserLogin data class contains email and password
+ * @file UserLogin.kt
+ * @author ksoundarya4
+ * @version 1.0
+ * @since 19/03/2020
+ */
+package com.bridgelabz.userloginapp
+
+data class UserLogin(val email: String, val password: String)
