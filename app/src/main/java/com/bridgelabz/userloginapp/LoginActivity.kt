f760789288cb8be4a7a05dca39b99cfae9509022
ksoundarya4 <ksoundarya4@gmail.com>
2020-03-19 19:30:08 +0530

    [soundarya]Refactor. isNetworkAvailable function to LoginActivity.kt

--- app/src/main/java/com/bridgelabz/userloginapp/LoginActivity.kt
+++ app/src/main/java/com/bridgelabz/userloginapp/LoginActivity.kt
@@ -1,10 +1,17 @@
 package com.bridgelabz.userloginapp
 
+import android.content.Context
 import android.content.Intent
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import androidx.constraintlayout.widget.ConstraintLayout
+
+import com.google.android.material.snackbar.Snackbar
 
 class LoginActivity : AppCompatActivity() {
 
@@ -24,6 +31,9 @@ class LoginActivity : AppCompatActivity() {
         findViewById<Button>(R.id.button_register)
     }
 
+    private val loginLayout by lazy {
+        findViewById<ConstraintLayout>(R.id.login_constaint_layout)
+    }
     private lateinit var httpRequestCall: HTTPRequestTask
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,6 +45,7 @@ class LoginActivity : AppCompatActivity() {
     private fun setButtonClickListeners() {
         setLoginButtonListener()
         setRegistrationButtonListener()
+
     }
 
 
@@ -42,8 +53,14 @@ class LoginActivity : AppCompatActivity() {
         loginButton.setOnClickListener {
             val email = emailEditText.text.toString()
             val password = passwordEditText.text.toString()
-            httpRequestCall = HTTPRequestTask()
-            httpRequestCall.execute()
+            val userLogin = UserLogin(email, password)
+            if (isNetworkAvailable()) {
+                httpRequestCall = HTTPRequestTask(userLogin)
+                httpRequestCall.execute()
+            } else
+                Snackbar.make(loginLayout, "No Internet Connection", Snackbar.LENGTH_LONG).apply {
+                    show()
+                }
         }
     }
 
@@ -56,5 +73,26 @@ class LoginActivity : AppCompatActivity() {
     private fun navigateToReciterActivity() {
         val intent = Intent(this, RegisterActivity::class.java)
         startActivity(intent)
+    }
+
+    private fun isNetworkAvailable(): Boolean {
+        val connectivityManager =
+            getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            val nw = connectivityManager.activeNetwork ?: return false
+            val actNw = connectivityManager.getNetworkCapabilities(nw) ?: return false
+            return when {
+                actNw.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+                actNw.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+                //for other device how are able to connect with Ethernet
+                actNw.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> true
+                //for check internet over Bluetooth
+                actNw.hasTransport(NetworkCapabilities.TRANSPORT_BLUETOOTH) -> true
+                else -> false
+            }
+        } else {
+            val nwInfo = connectivityManager.activeNetworkInfo ?: return false
+            return nwInfo.isConnected
+        }
     }
 }
