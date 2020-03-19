c7bf97adbc249b4d6c143f96f0d8fc1b1a3bee43
ksoundarya4 <ksoundarya4@gmail.com>
2020-03-19 17:06:07 +0530

    [soundarya]Refactor. Removed duplication of code from HTTPRequestTask.kt

--- app/src/main/java/com/bridgelabz/userloginapp/HTTPRequestTask.kt
+++ app/src/main/java/com/bridgelabz/userloginapp/HTTPRequestTask.kt
@@ -15,73 +15,64 @@ class HTTPRequestTask : AsyncTask<Void, Void, Void>() {
 
     override fun doInBackground(vararg params: Void?): Void? {
 
-        //GET
         try {
-            val url =
-                URL("http://fundoonotes.incubation.bridgelabz.com/api/user")
-            urlConnection = url.openConnection() as HttpURLConnection
-//            urlConnection!!.addRequestProperty("id", "5e71aee0ad53b700227c5905")
-            val responseCode = urlConnection!!.responseCode
 
-            if (responseCode != 200) {
-                throw IOException("Invalid response from Sever $responseCode")
-            }
+            performGETUsers()
+            performPOSTUserLogin()
 
-            val bufferedReader = BufferedReader(InputStreamReader(urlConnection!!.inputStream))
-
-            var line: String?
-            while (bufferedReader.readLine().also { line = it } != null) {
-                Log.i(tag, line!!)
-//                Log.d(tag, line.toString())
-            }
         } catch (exception: Exception) {
             exception.printStackTrace()
         } finally {
             if (urlConnection != null)
                 urlConnection!!.disconnect()
         }
-
-        // logIn
-        //POST
-        try {
-
-            val postData = JsonObject()
-            postData.addProperty("email", "ksoundarya4@gmail.com")
-            postData.addProperty("password", "sound7")
-
-            val url =
-                URL("http://fundoonotes.incubation.bridgelabz.com/api/user/login")
-            urlConnection = url.openConnection() as HttpURLConnection
-            urlConnection!!.setRequestProperty("Content-Type", "application/json")
-            urlConnection!!.requestMethod = "POST"
-            urlConnection!!.doOutput = true
-            urlConnection!!.doInput = true
-            urlConnection!!.setChunkedStreamingMode(0)
-
-            val outputStream = BufferedOutputStream(urlConnection!!.outputStream)
-            val writer = BufferedWriter(OutputStreamWriter(outputStream, "UTF-8"))
-            writer.write(postData.toString())
-            writer.flush()
-
-            val responseCode = urlConnection!!.responseCode
-            if (responseCode != 200) {
-                throw IOException("Invalid response from Sever $responseCode")
-            }
-
-            val bufferedReader = BufferedReader(InputStreamReader(urlConnection!!.inputStream))
-
-            var line: String?
-            while (bufferedReader.readLine().also { line = it } != null) {
-                Log.i(tag, line!!)
-//                Log.d(tag, line.toString())
-            }
-        } catch (exception: Exception) {
-            exception.printStackTrace()
-        } finally {
-            if (urlConnection != null)
-                urlConnection!!.disconnect()
-        }
-
         return null
+    }
+
+    private fun performPOSTUserLogin() {
+
+        val postData = JsonObject()
+        postData.addProperty("email", "ksoundarya4@gmail.com")
+        postData.addProperty("password", "sound7")
+
+        val urlAddress = "http://fundoonotes.incubation.bridgelabz.com/api/user/login"
+        urlConnection = getUrlConnection(urlAddress)
+
+        val outputStream = BufferedOutputStream(urlConnection!!.outputStream)
+        val writer = BufferedWriter(OutputStreamWriter(outputStream, "UTF-8"))
+        writer.write(postData.toString())
+        writer.flush()
+
+        getHttpResponse(urlConnection!!)
+    }
+
+    private fun performGETUsers() {
+
+        val urlAddress = "http://fundoonotes.incubation.bridgelabz.com/api/user"
+
+        urlConnection = getUrlConnection(urlAddress)
+
+        getHttpResponse(urlConnection!!)
+    }
+
+    private fun getHttpResponse(urlConnection: HttpURLConnection) {
+
+        val responseCode = urlConnection.responseCode
+
+        if (responseCode != 200) {
+            throw IOException("Invalid response from Sever $responseCode")
+        }
+
+        val bufferedReader = BufferedReader(InputStreamReader(urlConnection.inputStream))
+
+        var line: String?
+        while (bufferedReader.readLine().also { line = it } != null) {
+            Log.i(tag, line!!)
+        }
+    }
+
+    private fun getUrlConnection(urlAddress: String): HttpURLConnection {
+        val url = URL(urlAddress)
+        return url.openConnection() as HttpURLConnection
     }
 }
